d14fbdc1e15da5aa3a4f99301b98ef584c5b057b
Anna W <anski.anna@gmail.com>
2019-07-19 10:17:32 +0200

    added get all projects working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
+const db = require('./data/dbQueries');
 // const db = require('./data/dbQueries');
+// const projectRouter = require('./projects/project-router');
 
 const server = express();
 
@@ -8,6 +9,23 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send("hello world")
+})
+server.get('/projects', async (req, res) => {
+    try {
+        const projects = await db.getProjects();
+        res.json(projects);
+      } catch (err) {
+        res.status(500).json({ message: 'Failed to get the recipes' });
+      }
+})
+
+server.post('/', async (req, res) => {
+    try {
+        const project = await db.addPost(req.body);
+        res.status(201).json({project})
+    } catch (err) {
+        res.status(500).json({ error: 'could not create new post' })
+    }
 })
 
 module.exports = server;
