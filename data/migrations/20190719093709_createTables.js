b085a6e5e341810dc977014c7beeb68263e0192e
Anna W <anski.anna@gmail.com>
2019-07-19 09:45:00 +0200

    fixed bug. Migrations are up

---
+++ data/migrations/20190719093709_createTables.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', table => {
+        table.increments();
+        table.string('name', 128)
+            .notNullable();
+        table.string('description')
+            .notNullable();
+        table.boolean('completed')
+            .notNullable();
+    })
+    .createTable('actions', table => {
+        table.increments();
+        table.string('description')
+            .notNullable();
+        table.string('notes');
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+        table.boolean('completed')
+            .notNullable();
+    })
+
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('actions')
+    .dropTableIfExists('projects')
+};
